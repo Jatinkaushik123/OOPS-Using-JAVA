2ece9515d987ef0dc0634588f7d2ab17664bcb98
Jatin Kaushik <kaushikjatin2608@gmail.com>
2026-08-19 11:28:00 +0530

    Fix Lab_1 submodule to normal folder

---
+++ Lab_1/NameAndAddress.java
@@ -0,0 +1,6 @@
+public class NameAndAddress {
+    public static void main(String[] args) {
+        System.out.println("Name: Jatin Kaushik");
+        System.out.println("Address: BML Munjal Univeristy");
+    }
+}
